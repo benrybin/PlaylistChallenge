5ce1a7a53057c600a34f11ecd399d9b05883d483
Ben Rybinski <benrybinski@zipcoders-MacBook-Pro.local>
2019-05-31 15:55:15 -0400

    all done

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -9,6 +9,30 @@ public class Music {
     }
 
     public Integer selection(Integer startIndex, String selection){
-        return null;
+        int answer = 0;
+        int answer2 = 0;
+        int counter1 = 0;
+        int counter2 = 1;
+        int finalAnswer;
+
+        for(int i = startIndex;i < playList.length;i++){
+            if(selection == playList[i]){
+                 answer=counter1;
+                 break;
+            }
+            counter1++;
+        }
+        for(int j = startIndex;j < playList.length;j--){
+            if(j == 0){
+                j = playList.length-1;
+            }
+            if(selection == playList[j]){
+                answer2=counter2;
+                break;
+            }
+            counter2++;
+        }
+        finalAnswer = Math.min(answer,answer2);
+        return finalAnswer;
     }
 }
